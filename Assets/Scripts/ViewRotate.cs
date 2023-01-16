e50eca6096706076999a96444a518a8bf59998b2
Leijie Zhang <leijiezhang@uvic.ca>
2023-01-15 22:49:09 -0800

    GameDemo
    One ball with view rotation

---
+++ Assets/Scripts/ViewRotate.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class ViewRotate : MonoBehaviour
+{
+    //public GameObject cube;
+    Quaternion camaeraXAngle;//获取鼠标X轴偏移量当摄像机旋转角度
+    Quaternion camaeraYAngle;//获取鼠标Y轴偏移量当摄像机旋转角度
+ 
+ 
+    void LateUpdate()
+    {
+        if (Input.GetAxis("Mouse X") > 0.2)//当鼠标X轴偏移量大于0.3时执行，鼠标偏移量过小不执行(玩家也许只是想上下旋转摄像机但是难免会造成X轴偏移，所以当X轴偏移过小时不执行)
+        {
+            camaeraXAngle = Quaternion.Slerp(camaeraXAngle, Quaternion.Euler(0, Input.GetAxis("Mouse X"), 0), 0.1f);      //平滑设置旋转角度
+            transform.rotation *= camaeraXAngle;//旋转
+        }
+         else if (Input.GetAxis("Mouse X") < -0.2)
+        {
+           camaeraXAngle = Quaternion.Slerp(camaeraXAngle, Quaternion.Euler(0, Input.GetAxis("Mouse X"), 0), 0.1f);
+            transform.rotation *= camaeraXAngle;
+        }
+         if (Input.GetAxis("Mouse Y") < -0.2 && this.transform.rotation.x > 0.15)
+        {
+            camaeraYAngle = Quaternion.Slerp(camaeraYAngle, Quaternion.Euler(Input.GetAxis("Mouse Y"), 0, 0), 0.1f);
+            transform.rotation *= camaeraYAngle;
+        }
+        else if (Input.GetAxis("Mouse Y") > 0.2 && this.transform.rotation.x < 0.5)
+        {
+            camaeraYAngle = Quaternion.Slerp(camaeraYAngle, Quaternion.Euler(Input.GetAxis("Mouse Y"), 0, 0), 0.1f);
+            transform.rotation *= camaeraYAngle;
+        }
+        //this.transform.position = (this.transform.rotation * new Vector3(0, 0, -5) + cube.transform.position);//摄像机的位置设置
+        //this.transform.LookAt(cube.transform);//摄像机看向角色
+    }   
+}
